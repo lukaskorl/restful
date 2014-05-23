6f316316fc8c47e431c88af18d0efa8d115384de
Lukas Korl <hello@lukaskorl.com>
2014-05-23 21:34:02 +0200

    Config factory for decoupling from Laravel

---
+++ src/Lukaskorl/Restful/ConfigFactory.php
@@ -0,0 +1,88 @@
+<?php namespace Lukaskorl\Restful;
+
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Config\FileLoader;
+use Illuminate\Config\Repository;
+
+class ConfigFactory {
+
+    public static $configPath = null;
+    public static $env = "production";
+
+    protected static $configs = array();
+
+    public static function make($configPath = false, $environment = false)
+    {
+        // Filter input
+        if ($configPath === false) $configPath = self::getConfigPath();
+        if ($environment === false) $environment = self::getEnvironment();
+
+        // Check if there is already an instance for this configuration
+        $identifier = self::getIdentifier($configPath, $environment);
+        if (isset(self::$configs[$identifier])) return self::$configs[$identifier];
+
+        // Create the configuration
+        $file = new Filesystem;
+        $loader = new FileLoader($file, $configPath);
+        self::$configs[$identifier] = new Repository($loader, $environment);
+        return self::$configs[$identifier];
+    }
+
+    /**
+     * Return a unique identifier for the current setup
+     *
+     * @param bool $configPath
+     * @param bool $environment
+     * @return string
+     */
+    public static function getIdentifier($configPath = false, $environment = false)
+    {
+        // Filter input
+        if ($configPath === false) $configPath = self::getConfigPath();
+        if ($environment === false) $environment = self::getEnvironment();
+
+        return md5($configPath."@".$environment);
+    }
+
+    /**
+     * Get the environment set on the factory
+     *
+     * @return string
+     */
+    public static function getEnvironment()
+    {
+        return self::$env;
+    }
+
+    /**
+     * Set the environment on the factory
+     *
+     * @param $env
+     */
+    public static function setEnvironment($env)
+    {
+        self::$env = $env;
+    }
+
+    /**
+     * Get the current config path
+     *
+     * @return null|string
+     */
+    public static function getConfigPath()
+    {
+        if (self::$configPath) return self::$configPath;
+        return __DIR__."/../../config";
+    }
+
+    /**
+     * Set the config path on this factory
+     *
+     * @param $path
+     */
+    public static function setConfigPath($path)
+    {
+        self::$configPath = $path;
+    }
+
+} 
